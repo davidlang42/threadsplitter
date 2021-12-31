c61d4eadf634c680c4fbdd0c817a0e131f976f9d
David Lang <davidlang42@gmail.com>
2021-12-31 17:25:46 +1100

    Add existing code from apps scripts

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# threadsplitter
-An add-on to split GMail threads
+# ThreadSplitter
+An add-on to split GMail threads.
+
+GMail conversation threads are great, but sometimes stupid companies send you multiple emails about different things (eg. orders) with an IDENTICAL subject line. These get automatically made into threads because GMail doesn't know any better, so this add-on lets you split them up when required.
+
+The add-on is hosted directly on Google Apps Script, free for anyone to use: (Google Workspace add-on url TBA)
+
+But by all means, [buy me a coffee](https://ko-fi.com/davidlang42).
+
+## Set up local repo
+* Clone git repo: `git clone https://github.com/davidlang42/threadsplitter.git`
+* Install [clasp](https://developers.google.com/apps-script/guides/clasp): `npm install @google/clasp -g`
+* Login to clasp: `clasp login`
+* Enter app directory: `cd app`
+* Connect apps script project: `clasp clone [scriptId]`
+
+## Deploying changes
+### Use bash script
+* Run from the root of the repo: `./deploy.sh`
+  * Warning: This will overwrite any changes made directly on google apps scripts, but they will still exist in a reverted commit labelled 'possible lost changes'
+### Execute manually
+* Enter app directory: `cd app`
+* Pull changes to local git repo: `git pull`
+* Push changes to apps scripts: `clasp push`
+  * Warning: This will overwrite any changes made directly on google apps scripts
+* Find existing deployment: `clasp deployments`
+  * Returns deployment id: `- AKfycby34UVLubaiUs-68eAwC1qD9Pl0_W05ZzghoSOeOZZ2 @58 - Test via Clasp`
+* Create version & update existing deployment: `clasp deploy -i [deploymentId] -d "[description]"`
---
+++ app/Addon.js
@@ -0,0 +1,110 @@
+//TODO use a better icon than script
+
+function onGmailMessageOpen(e) {
+  // Activate temporary Gmail scopes, in this case to allow
+  // the add-on to read message metadata and content.
+  var accessToken = e.gmail.accessToken;
+  GmailApp.setCurrentMessageAccessToken(accessToken);
+
+  // Read message metadata and content. This requires the Gmail scope
+  // https://www.googleapis.com/auth/gmail.addons.current.message.readonly.
+  var messageId = e.gmail.messageId;
+  var message = GmailApp.getMessageById(messageId);
+
+  // Setting the access token with a gmail.addons.current.message.readonly
+  // scope also allows read access to the other messages in the thread.
+  var thread = message.getThread();
+  var threadMessages = thread.getMessages();
+
+  // Can only split thread if at least 2 messages
+  if (threadMessages.length < 2)
+    return [];
+
+  // Construct the card
+  const input = CardService.newSelectionInput()
+    .setType(CardService.SelectionInputType.CHECK_BOX)
+    .setFieldName("messages")
+    .setTitle("Messages in new thread");
+  for (const m of threadMessages) {
+    if (!m.isInTrash())
+      input.addItem(m.getFrom() + " at " + formatDate(m.getDate()), m.getId(), m.getId() == messageId);
+    //TODO select the last item by default
+  }
+
+  const defaultSubject = thread.getFirstMessageSubject() + ' (Split)';
+  var section = CardService.newCardSection();
+  section.addWidget(CardService.newTextInput()
+      .setTitle("Subject of new thread")
+      .setFieldName("subject")
+      .setValue(defaultSubject))
+    .addWidget(input)
+    .addWidget(CardService.newButtonSet()
+      .addButton(CardService.newTextButton()
+        .setText('Split thread')
+        .setOnClickAction(CardService.newAction()
+          .setFunctionName('onSplitThreadButtonClick'))));
+
+  var builder = CardService.newCardBuilder()
+    .addSection(section);
+  return builder.build();
+}
+
+function onSplitThreadButtonClick(e) {
+  //TODO is this required?
+  var accessToken = e.gmail.accessToken;
+  GmailApp.setCurrentMessageAccessToken(accessToken);
+
+  var subject = e.formInput.subject;
+  if (subject.length == 0)
+    return notify("Please enter a subject");
+
+  var messages = e.formInputs.messages;
+  var text;
+  if (messages.length == 0)
+    return notify("Please select at least one message");
+  else if (messages.length == 1)
+    text = "Split 1 message into a new thread";
+  else
+    text = "Split " + messages.length + " messages into a new thread";
+
+  splitThread(subject, messages);
+
+  var actionResponse = CardService.newActionResponseBuilder()
+    .setNavigation(CardService.newNavigation()
+      .updateCard(CardService.newCardBuilder()
+        .addSection(CardService.newCardSection()
+          .addWidget(CardService.newTextParagraph()
+            .setText(text)))
+        .build()));
+  return actionResponse.build();
+}
+
+function notify(text) {
+  var notification = CardService.newNotification()
+    .setText(text);
+  var actionResponse = CardService.newActionResponseBuilder()
+    .setNotification(notification);
+  return actionResponse.build();
+}
+
+function splitThread(subject, messageIds) {
+  const recipient = Session.getActiveUser().getEmail();
+  for (const id of messageIds) {
+    const m = GmailApp.getMessageById(id);
+    var replyTo = m.getReplyTo();
+    if (!replyTo || replyTo == '')
+      replyTo = m.getFrom();
+    GmailApp.sendEmail(recipient, subject + m.getReplyTo(), m.getPlainBody(), {
+      htmlBody: m.getBody(),
+      attachments: m.getAttachments(),
+      name: m.getFrom(),
+      replyTo: replyTo
+    });
+    m.moveToTrash();
+  }
+}
+
+function formatDate(dateString) {
+  var d = new Date(dateString);
+  return Utilities.formatDate(d, Session.getScriptTimeZone(), "dd MMM YYYY, HH:mm");
+}
